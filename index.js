c595e8cbc088a4ca41a27bd3ee5b48eb59b491eb
ligalaz <95artm.mick@gmail.com>
2023-01-16 12:40:33 +0300

    feat:add modal & slider

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+import Modal from "./modal.js";
+
+const modal = new Modal(document.querySelector(`.modal`, 0));
+modal.initialize();
+
 const burger = document.querySelector(".burger");
 
 const navigation = document.querySelector(".header__navigation");
-
-console.log(navigation);
 
 burger.addEventListener("click", () => {
   navigation.classList.toggle("active");
